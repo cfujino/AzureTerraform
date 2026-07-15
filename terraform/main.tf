54ab7920d262377250d11b7cf339cf0e90c51260
cfujino <154095363+cfujino@users.noreply.github.com>
2026-07-14 16:47:07 -1000

    Refactor resource group and subnet configurations
    Updated resource group and subnet configurations to use variables for resource group name and location. Fixed typo in virtual network resource name.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,3 @@
-resource "azurerm_resource_group" "main" {
-  name     = "devsecops-test-rg-prod"
-  location = "East US"
-}
 
 resource "azurerm_resource_group" "main"{
   name = var.resource_group_name
@@ -14,12 +10,13 @@ resource "azurerm_virtual_network" "main" {
     "10.0.0.0/16"
   ]
   location = azurerm_resource_group.main.location
-resource_group_name = azurerm_resource_group.main.name
+  resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "main" {
+  name = "ollama-subnet"
   resource_group_name = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtial_network.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes = [
     "10.0.1.0/24"
   ]
