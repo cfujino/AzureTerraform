0e750019c869da9b4a9d7fd2811398eb58d30be2
cfujino <154095363+cfujino@users.noreply.github.com>
2026-07-14 15:48:18 -1000

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "rg-ollama-ai"
+}
+
+variable "vm_name" {
+  default = "ollama-vm"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
